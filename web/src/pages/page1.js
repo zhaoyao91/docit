0fd522498a4fa0977c733df545f819d7b0095b37
zhaoyao91 <zhaoyao91@163.com>
2017-03-17 22:24:27 +0800

    rename webpack config

--- web/src/pages/page1.js
+++ web/src/pages/page1.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Button } from 'semantic-ui-react'
+
 import Hello from '../views/hello'
 
 export default () => (
   <div>
     <h1>Page 1</h1>
     <Hello/>
+    <Button primary>Primary</Button>
   </div>
 )
