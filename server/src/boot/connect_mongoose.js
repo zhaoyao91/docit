dff9c118647b1b8c62a4a879b51d5a916814e78e
zhaoyao91 <zhaoyao91@163.com>
2017-03-19 12:25:11 +0800

    remove mongoose

--- server/src/boot/connect_mongoose.js
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose'
-
-export default async () => {
-  // plugin Promise implementation, see http://mongoosejs.com/docs/promises.html
-  mongoose.Promise = global.Promise;
-
-  return await mongoose.createConnection('mongodb://127.0.0.1:27017/docit')
-}
--- server/src/index.js
+++ server/src/index.js
@@ -1,11 +1,5 @@
-import path from 'path'
-
-import connectMongoose from './boot/connect_mongoose'
-import createModels from './boot/create_models'
-
 async function boot () {
-  const mongoose = await connectMongoose()
-  const models = await createModels(mongoose, path.resolve(__dirname, './models'))
+
 }
 
 boot().catch(err => {
